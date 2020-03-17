b46882301fdecc204b739f26915f1f2f2ca90cc1
Aprajita-96 <aprajithabajpai@gmail.com>
2020-03-17 17:49:46 +0530

    new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EnglishComponent } from './english/english.component';
+import { SpanishComponent } from './spanish/spanish.component';
+import { I18nModule } from './i18n/i18n.module';
+import { SelectLanguageComponent } from './select-language/select-language.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnglishComponent,
+    SpanishComponent,
+    SelectLanguageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    I18nModule
   ],
   providers: [],
   bootstrap: [AppComponent]
